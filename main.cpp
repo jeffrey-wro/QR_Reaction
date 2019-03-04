7d6b65a97ef03eabe816c4ddcae52793a12006aa
ArmandoAren1 <armandoarenas14@gmail.com>
2019-03-04 10:42:49 -0800

    demo file

--- main.cpp
+++ main.cpp
@@ -49,7 +49,7 @@ int main(int argc, char **argv) {
     Mat display, frame, pts;
 	
 	for(;;)
-    {
+	{
 		cam >> frame; //get new frame
 		cvtColor(frame, display, COLOR_BGR2GRAY);
 		
@@ -59,20 +59,49 @@ int main(int argc, char **argv) {
 			//set data to decoded qr code
 			data = qrDecoder.detectAndDecode(display, pts);
 			
-			if(data == "Ver1")
+			if(data == "Forwards") //forwards
 			{
 				//set motor speed
 				mc.setMotorSpeeds(DC, 50, -50);
-				//wait for 2 seconds
-				Utils::waitFor(2);
+				//wait for 3 seconds
+				Utils::waitFor(3);
 				//set speed 0
 				mc.setMotorSpeeds(DC, 0, 0);
-				break;
 			}
+			if(data == "Square") //move in a square
+			{
+				int leftCount = 0;
+				int rightCount = 0;
+				
+				for(int i=0; i<4; i++){
+					//move forward
+					leftCount += 360;
+					rigthCount -= 360;
+					mc.setMotorDegrees(DC, speed, leftCount, speed, 					rigthCount);
+					//wait for 3 seconds
+					Utils::waitFor(3);
+
+					//turn 90 degrees
+					rigthCount -= 465;
+					mc.setMotorDegrees(DC, 0, leftCount, speed, rigthCount);
+					Utils::waitFor(3);
+				}
+			}
+			if(data == "Backwards") //backwards
+			{
+				//set motor speed
+				mc.setMotorSpeeds(DC, -50, 50);
+				//wait for 3 seconds
+				Utils::waitFor(3);
+				//set speed 0
+				mc.setMotorSpeeds(DC, 0, 0);
+			}
+			
+			//delay and reset
+			data = "0";
 		}
 
 	}
-
 	Utils::waitFor(2);
 
 	mc.controllerReset(DC);
